1ec996228f51da4b7b22f0d00febb6b85b1bd522
Maxim Kuzin <BountyMaxim@yandex.ru>
2023-11-30 18:05:58 +0300

    add builder

--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -5,3 +5,43 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+
+type Builder interface {
+	MakeHeader(str string)
+	MakeBody(str string)
+	MakeFooter(str string)
+}
+
+type Director struct {
+	builder Builder
+}
+
+func (d *Director) Construct() {
+	d.builder.MakeHeader("Header")
+	d.builder.MakeBody("Body")
+	d.builder.MakeFooter("Footer")
+}
+
+type ConcreteBuilder struct {
+	product *Product
+}
+
+func (b *ConcreteBuilder) MakeHeader(str string) {
+	b.product.Content += "<header>" + str + "</header>"
+}
+
+func (b *ConcreteBuilder) MakeBody(str string) {
+	b.product.Content += "<article>" + str + "</article>"
+}
+
+func (b *ConcreteBuilder) MakeFooter(str string) {
+	b.product.Content += "<footer>" + str + "</footer>"
+}
+
+type Product struct {
+	Content string
+}
+
+func (p *Product) Show() string {
+	return p.Content
+}
